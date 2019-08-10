1f0fe3701dce63f62f99dba138a989c63fe0e939
Yuan Ji <yuan.ji@jiwhiz.com>
2019-08-10 12:43:22 -0600

    Use Bootstrap breakpoints to classify device

---
+++ src/UiFramework/ResponsiveUtils.elm
@@ -0,0 +1,30 @@
+module UiFramework.ResponsiveUtils exposing (..)
+
+import Element exposing (Device, DeviceClass(..), Orientation(..))
+
+
+
+-- Use Bootstrap responsive breakpoints: https://getbootstrap.com/docs/4.3/layout/overview/#responsive-breakpoints
+
+
+classifyDevice : { window | height : Int, width : Int } -> Device
+classifyDevice window =
+    { class =
+        if window.width < 768 then
+            Phone
+
+        else if window.width < 992 then
+            Tablet
+
+        else if window.width < 1200 then
+            Desktop
+
+        else
+            BigDesktop
+    , orientation =
+        if window.width < window.height then
+            Portrait
+
+        else
+            Landscape
+    }
